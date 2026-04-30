8b0a539a83fe2bb4832f63acbb4c94bf113c8b09
Hasan Gökay Günaydın <hasan@MacBook-Pro.local>
2026-04-30 13:35:37 +0300

    Hesap ile giriş eklendi.

--- claudey/users/views.py
+++ claudey/users/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.contrib import messages
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import redirect, render
 
-# Create your views here.
+
+def register(request):
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            messages.success(request, "Hesabınız oluşturuldu.")
+            return redirect("home")
+    else:
+        form = UserCreationForm()
+
+    return render(request, "users/register.html", {"form": form})
